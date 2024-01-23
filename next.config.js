132461e03a95070e38db555d0fe71e87ca9c995c
imunika <eli.novoa@gmail.com>
2024-01-22 19:54:14 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig}  */
 module.exports = {
   output: 'export',
+  images: {
+    loader: "akamai",
+    path: "",
+  },
   basePath: '/TMPL',
   assetPrefix: '/TMPL',
 };
